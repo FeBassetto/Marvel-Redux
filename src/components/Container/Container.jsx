08d042adb53fbf01109692bf545aba374f01ede3
Felipe Bassetto <felipebtu9@gmail.com>
2022-05-20 22:12:51 -0300

    Utilizando styled components no Container

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import style from './Container.module.css'
+import styled from 'styled-components'
+
+const StyledContainer = styled.main`
+background-color: ${props => props.theme.background}
+`
 
 const Container = ({children}) => {
     return(
-        <main className={style.container}>
+        <StyledContainer className={style.container}>
             {children}
-        </main>
+        </StyledContainer>
     )
 }
 
