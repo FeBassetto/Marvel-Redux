709720953356e96ad38638d08f9daf364d8d8cbe
Felipe Bassetto <felipebtu9@gmail.com>
2022-05-26 15:38:20 -0300

    Saving favorites on localStorage

---
+++ src/store/localStorage.js
@@ -0,0 +1,22 @@
+export const loadState = () => {
+    try {
+        const localStorageState = localStorage.getItem('favoriteReducer')
+        if (localStorageState === null) {
+            return undefined
+        }
+        return JSON.parse(localStorageState)
+    } catch (err) {
+        console.log('Nao foi possivel recuperar o estado', err)
+        return undefined
+    }
+}
+
+export const saveState = state => {
+    try {
+        const localStorageState = JSON.stringify(state)
+        console.log(state)
+        localStorage.setItem('favoriteReducer', localStorageState)
+    } catch (err) {
+        console.log('Nao foi possivel salvar o estado', err);
+    }
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from 'redux-saga'
+import { loadState, saveState } from "./localStorage";
 
 import reducers from './reducers/reducers'
 import saga from "./saga/saga";
@@ -7,12 +8,18 @@ import saga from "./saga/saga";
 const sagaMiddleware = createSagaMiddleware()
 const enhancer = applyMiddleware(sagaMiddleware)
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const localStorage = loadState()
 
 const store = createStore(
     reducers,
+    localStorage,
     composeEnhancers(enhancer)
 )
 
 sagaMiddleware.run(saga)
+
+store.subscribe(() => {
+    saveState({favoriteReducer: store.getState().favoriteReducer})
+})
 
 export default store
