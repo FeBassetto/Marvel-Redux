76ba8a8ffbd09bb7c0355bb6bd15bb096c473dac
Felipe Bassetto <felipebtu9@gmail.com>
2022-05-29 15:58:01 -0300

    Adicionando banner rotativo

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,19 +1,40 @@
 import React from "react";
 import style from './Banner.module.css'
 import bannerImage from '../../assets/imgs/bannerImage.png'
+import marvelLogo from '../../assets/imgs/marvelLogo.jpeg'
 import { Link } from "react-router-dom";
+import { Carousel } from 'react-bootstrap';
+
 
 const Banner = () => {
-    return(
+    return (
         <section className={style.banner}>
-            <img 
-            src={bannerImage} 
-            alt="Banner Heroes" 
-                className={style.banner__image}
-            />
-            <Link to='/' className={style.banner__link}>
-                <button>Veja lançamento Loki</button>
-            </Link>
+            <Carousel>
+                
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={marvelLogo}
+                        alt="Second slide"
+                    />
+                    <Carousel.Caption>
+                        <p className={style.banner__title}>MARVEL HERO</p>
+                        <h2 className={style.banner__subTitle}>Descubra e pesquise sobre todos seus heróis favoritos</h2>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={bannerImage}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <Link to='heroes/1009407' className={style.banner__link}>
+                            <button>Veja mais sobre novo herói Loki</button>
+                        </Link>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </section>
     )
 }
